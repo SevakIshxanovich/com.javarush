feab5abde36682dda6b3ed6103cd46699d05b696
Sevak <petrosyansevak@gmail.com>
2021-03-31 09:53:23 +0300

    My commit

---
+++ src/tests/Integers.java
@@ -0,0 +1,50 @@
+package tests;
+
+public class Integers {
+        /**
+
+         ------------------------------------
+         System.out.println(Integer.TYPE);
+         System.out.println(Integer.SIZE);
+         System.out.println(Integer.MIN_VALUE);
+         System.out.println(Integer.MAX_VALUE);
+         --------------------------------------
+         System.out.println(Arrays.toString(s));
+         ---------------------------------------
+         for (int[] result1 :result) {
+          System.out.println(Arrays.toString(result1)); }
+         ------------------------------------------------
+         for (int i = 10; i > 0; i--)
+                {
+                    System.out.print(i);
+                    Thread.sleep(1000);
+                }
+                System.out.println("Поехали");
+         ----------------------------------------------------------------------------
+         width                     minimum                         maximum
+
+         SIGNED
+         byte:     8 bit                        -128                            +127
+         short:   16 bit                     -32 768                         +32 767
+         int:     32 bit              -2 147 483 648                  +2 147 483 647
+         long:    64 bit  -9 223 372 036 854 775 808      +9 223 372 036 854 775 807
+
+         UNSIGNED
+         char     16 bit                           0                         +65 535
+         ----------------------------------------------------------------------------
+         width                     minimum                         maximum
+         signed    8 bit                        -128                            +127
+         signed   16 bit                     -32 768                         +32 767
+         signed   32 bit              -2 147 483 648                  +2 147 483 647
+         signed   64 bit  -9 223 372 036 854 775 808      +9 223 372 036 854 775 807
+         unsigned  8 bit                           0                            +255
+         unsigned 16 bit                           0                         +65 535
+         unsigned 32 bit                           0                  +4 294 967 295
+         unsigned 64 bit                           0     +18 446 744 073 709 551 615
+         The order is: / byte 8 bits/ , / short 16 bits/ , / int 32 bits/ , / long 64 bits/.
+         -----------------------------------------------------------------------------------
+         int i =Integer.parseUnsignedInt("4000000");
+         ------------------------------------------------------------------------------------
+         */
+
+}
