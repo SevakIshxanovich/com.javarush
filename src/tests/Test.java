3fdded2d15d941105fa6c66a2fdd10790068c196
Sevak <petrosyansevak@gmail.com>
2021-04-11 21:55:12 +0300

    My commit

--- src/tests/Test.java
+++ src/tests/Test.java
@@ -1,27 +1,35 @@
 package tests;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
-import java.lang.management.GarbageCollectorMXBean;
-import java.util.*;
-import java.io.*;
-
-public class Test {
-
-    public static void main(String[] args) {
-
-
-        System.out.println(Double.POSITIVE_INFINITY);
-        System.out.println(Double.NEGATIVE_INFINITY);
-        System.out.println(Double.NaN);
-        System.out.println(Double.POSITIVE_INFINITY/Double.NEGATIVE_INFINITY);
-
-//        Child child = new Child();
-//        child = null;
-//        System.gc();
+public class Test{
+    public static int[] max(int...a){
+        int i[] = new int[2];
+        for (int b:a) {
+            if (b > 0) {
+                i[0]++;
+            } else {
+                i[1]++;
+            }
+        }
+            return i;
 
     }
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        System.out.print("Введите каличество чисел которые надо проверить: ");
+        int index = Integer.parseInt(reader.readLine());
+        int mas[] = new int[index];
+
+        for (int j=0; j<index; j++) {
+            System.out.print("Введите " + (j+1) + "-e число: ");
+            mas[j] = Integer.parseInt(reader.readLine());
+        }
+
+        System.out.println("Введено " + max(mas)[0] + " положительных числа \n" +
+                           "Введено " + max(mas)[1] + " отрицателных числа");
+    }
 }
-
-/*
-
- */
